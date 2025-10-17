40612b6b0f266b2c53320d5948a9eb72f81f8d93
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-10-17 14:08:27 +0300

    app colors done

---
+++ lib/Core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color titleActive = Color(0xff000000);
+  static const Color body = Color(0xff333333);
+  static const Color label = Color(0xff555555);
+  static const Color placeholder = Color(0xff888888);
+  static const Color line = Color(0xffE0CFBA);
+  static const Color inputBackground = Color(0xffF9F9F9);
+  static const Color background = Color(0xffF8F0E7);
+  static const Color offWhite = Color(0xffFCFCFC);
+  static const Color primary = Color(0xffA8715A);
+  static const Color secondary = Color(0xffDD8560);
+}
