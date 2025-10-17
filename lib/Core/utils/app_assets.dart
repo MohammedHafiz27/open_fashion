37ee11131285d870f8709ccf57b9e868e163d0f1
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-10-17 14:23:09 +0300

    app assets done

---
+++ lib/Core/utils/app_assets.dart
@@ -0,0 +1,161 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for iconsBackward
+  /// assets/icons/Backward.svg
+  static const String iconsBackward = "assets/icons/Backward.svg";
+
+  /// Assets for iconsCall
+  /// assets/icons/Call.svg
+  static const String iconsCall = "assets/icons/Call.svg";
+
+  /// Assets for iconsClose
+  /// assets/icons/Close.svg
+  static const String iconsClose = "assets/icons/Close.svg";
+
+  /// Assets for iconsDoNotBleach
+  /// assets/icons/DoNotBleach.svg
+  static const String iconsDoNotBleach = "assets/icons/DoNotBleach.svg";
+
+  /// Assets for iconsDoNotTumbleDry
+  /// assets/icons/DoNotTumbleDry.svg
+  static const String iconsDoNotTumbleDry = "assets/icons/DoNotTumbleDry.svg";
+
+  /// Assets for iconsDoNotWash
+  /// assets/icons/DoNotWash.svg
+  static const String iconsDoNotWash = "assets/icons/DoNotWash.svg";
+
+  /// Assets for iconsDoortoDoorDelivery
+  /// assets/icons/DoortoDoorDelivery.svg
+  static const String iconsDoortoDoorDelivery = "assets/icons/DoortoDoorDelivery.svg";
+
+  /// Assets for iconsDown
+  /// assets/icons/Down.svg
+  static const String iconsDown = "assets/icons/Down.svg";
+
+  /// Assets for iconsExport
+  /// assets/icons/Export.svg
+  static const String iconsExport = "assets/icons/Export.svg";
+
+  /// Assets for iconsFilter
+  /// assets/icons/Filter.svg
+  static const String iconsFilter = "assets/icons/Filter.svg";
+
+  /// Assets for iconsForward
+  /// assets/icons/Forward.svg
+  static const String iconsForward = "assets/icons/Forward.svg";
+
+  /// Assets for iconsForwardArrow
+  /// assets/icons/ForwardArrow.svg
+  static const String iconsForwardArrow = "assets/icons/ForwardArrow.svg";
+
+  /// Assets for iconsGallery
+  /// assets/icons/Gallery.svg
+  static const String iconsGallery = "assets/icons/Gallery.svg";
+
+  /// Assets for iconsGridview
+  /// assets/icons/gridview.svg
+  static const String iconsGridview = "assets/icons/gridview.svg";
+
+  /// Assets for iconsHeart
+  /// assets/icons/Heart.svg
+  static const String iconsHeart = "assets/icons/Heart.svg";
+
+  /// Assets for iconsIronLowTemperature
+  /// assets/icons/IronLowTemperature.svg
+  static const String iconsIronLowTemperature = "assets/icons/IronLowTemperature.svg";
+
+  /// Assets for iconsListview
+  /// assets/icons/Listview.svg
+  static const String iconsListview = "assets/icons/Listview.svg";
+
+  /// Assets for iconsLocation
+  /// assets/icons/Location.svg
+  static const String iconsLocation = "assets/icons/Location.svg";
+
+  /// Assets for iconsLogo
+  /// assets/icons/Logo.svg
+  static const String iconsLogo = "assets/icons/Logo.svg";
+
+  /// Assets for iconsMenu
+  /// assets/icons/Menu.svg
+  static const String iconsMenu = "assets/icons/Menu.svg";
+
+  /// Assets for iconsPlus
+  /// assets/icons/Plus.svg
+  static const String iconsPlus = "assets/icons/Plus.svg";
+
+  /// Assets for iconsRefresh
+  /// assets/icons/Refresh.svg
+  static const String iconsRefresh = "assets/icons/Refresh.svg";
+
+  /// Assets for iconsResize
+  /// assets/icons/Resize.svg
+  static const String iconsResize = "assets/icons/Resize.svg";
+
+  /// Assets for iconsSearch
+  /// assets/icons/Search.svg
+  static const String iconsSearch = "assets/icons/Search.svg";
+
+  /// Assets for iconsShipping
+  /// assets/icons/Shipping.svg
+  static const String iconsShipping = "assets/icons/Shipping.svg";
+
+  /// Assets for iconsShopping bag
+  /// assets/icons/shopping bag.svg
+  static const String iconsShopping bag = "assets/icons/shopping bag.svg";
+
+  /// Assets for iconsTag
+  /// assets/icons/Tag.svg
+  static const String iconsTag = "assets/icons/Tag.svg";
+
+  /// Assets for iconsUp
+  /// assets/icons/Up.svg
+  static const String iconsUp = "assets/icons/Up.svg";
+
+  /// Assets for iconsVoucher
+  /// assets/icons/Voucher.svg
+  static const String iconsVoucher = "assets/icons/Voucher.svg";
+
+  /// Assets for productsCover1
+  /// assets/products/cover1.png
+  static const String productsCover1 = "assets/products/cover1.png";
+
+  /// Assets for productsCover2
+  /// assets/products/cover2.png
+  static const String productsCover2 = "assets/products/cover2.png";
+
+  /// Assets for productsCover3
+  /// assets/products/cover3.png
+  static const String productsCover3 = "assets/products/cover3.png";
+
+  /// Assets for productsProduct1
+  /// assets/products/product1.png
+  static const String productsProduct1 = "assets/products/product1.png";
+
+  /// Assets for productsProduct2
+  /// assets/products/product2.png
+  static const String productsProduct2 = "assets/products/product2.png";
+
+  /// Assets for productsProduct3
+  /// assets/products/product3.png
+  static const String productsProduct3 = "assets/products/product3.png";
+
+  /// Assets for productsProduct4
+  /// assets/products/product4.png
+  static const String productsProduct4 = "assets/products/product4.png";
+
+  /// Assets for productsProduct5
+  /// assets/products/product5.png
+  static const String productsProduct5 = "assets/products/product5.png";
+
+  /// Assets for productsProduct6
+  /// assets/products/product6.png
+  static const String productsProduct6 = "assets/products/product6.png";
+
+  /// Assets for productsRecycleBoucleKnitCardigan Pink
+  /// assets/products/RecycleBoucleKnitCardigan Pink.png
+  static const String productsRecycleBoucleKnitCardigan Pink = "assets/products/RecycleBoucleKnitCardigan Pink.png";
+}
+
